3d88febce91e2136a5a908eb905994169daa5c71
sjcnewpc <luxdolorosa@naver.com>
2019-06-23 04:44:02 +0900

    init

---
+++ SemiProject2/src/com/test/Test2.java
@@ -0,0 +1,24 @@
+package com.test;
+
+/**
+ * 클래스에 대한 설명을 여기에 쓴다.
+ * 
+ * @author 	sjcnew
+ * @since 	2019. 6. 23.
+ * @version 1.0
+ * @see <pre>
+ *  == 개정이력(Modification Information) ==
+ *   
+ *   수정일			수정자				수정내용
+ *  ---------------------------------------------------------------------------------
+ *   2019. 6. 23.		sjcnew				최초생성
+ * 
+ * </pre>
+ */
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("두번째 테스트 파일");
+	}
+
+}
